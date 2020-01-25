0f1ce39c5a5586b5770bea75227bc2703d3ce95d
Adam Harner <adamj.harner@gmail.com>
2020-01-25 14:23:01 -0500

    Updated HabitModel.java

--- HabitModel.java
+++ HabitModel.java
@@ -2,5 +2,36 @@ package com.example.habit;
 
 public class HabitModel {
 
+    private double originalTime;
+    private double currentTime;
+    private double expectedTime;
+    private int timeFrame;
+    private String name;
+
+    public HabitModel(String n, String tF, String oT, String cT, String eT) {
+        this.originalTime = Double.parseDouble(oT);
+        this.currentTime = Double.parseDouble(cT);
+        this.expectedTime = Double.parseDouble(eT);
+        this.timeFrame = Integer.parseInt(tF);
+        this.name = n;
+
+
+    }
+
+    public String printHabit(){
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+        HabitModel myHabit = new HabitModel("Yoga", "14", "5", "5", "30");
+        System.out.println()
+
+    }
+
+
+
 
 }
