a31060087e9efcc5dbec06170d534edf4b737c03
yulongkelly <51765788+yulongkelly@users.noreply.github.com>
2020-01-25 18:42:28 -0500

    Add files via upload

--- HabitModel.java
+++ HabitModel.java
@@ -1,41 +1,53 @@
-package com.example.habit;
+package com.example.deltahack;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HabitModel {
 
     private double originalTime;
-    private double currentTime;
+    protected List<Double> currentTimes = new ArrayList<>();;
     private double expectedTime;
     private int timeFrame;
     private String name;
 
-    public HabitModel(String n, String tF, String oT, String cT, String eT) {
+    public HabitModel(String n, String tF, String oT, String eT) {
         this.originalTime = Double.parseDouble(oT);
-        this.currentTime = Double.parseDouble(cT);
         this.expectedTime = Double.parseDouble(eT);
         this.timeFrame = Integer.parseInt(tF);
         this.name = n;
-
-
+        currentTimes.add(this.originalTime);
     }
 
     public String printHabit(){
         double originalTime = this.originalTime;
-        double currentTime = this.currentTime;
         double expectedTime = this.expectedTime;
         int timeFrame = this.timeFrame;
         String name = this.name;
 
-        String result = String.format("Your new habit is " + name + "." + " Current time spent per day is: " + currentTime + " hours, Goal time per day is: "
+        String result = String.format("Your new habit is " + name + "." + " Current time spent per day is: " + originalTime + " hours, Goal time per day is: "
         + expectedTime + "hours, Time-Frame for this habit is " +timeFrame + " days.");
 
         return result;
 
     }
 
+    public void printCurrentTimes() {
+        for(Double currentTime: currentTimes) {
+            System.out.println(currentTime + " ");
+        }
+    }
 
+    /** print the average current time the user spending on the target activities from
+     * start to current
+     * @return
+     */
+//    public String printAverage() {
+//        if(currentTimes.size() > 3) {
+//
+//        }
+//    }
     public static void main(String[] args) {
-        HabitModel myHabit = new HabitModel("Yoga", "14", "5", "5", "30");
-        System.out.println(myHabit.printHabit());
 
     }
 
--- MainActivity.java
+++ MainActivity.java
@@ -1,11 +1,17 @@
-package com.example.habit;
+package com.example.deltahack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.example.deltahack.gainHabbit;
+import com.example.deltahack.R;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    HabitModel myHabit;
+
     public void buttonClicked(View v){
 
         EditText nameView = (EditText) findViewById(R.id.habitName);
@@ -24,13 +32,29 @@ public class MainActivity extends AppCompatActivity {
         String originalTime = originalTimeView.getText().toString();
         EditText expectedTimeView = (EditText) findViewById(R.id.expectedTimeBox);
         String expectedTime = expectedTimeView.getText().toString();
-        String currentTime = originalTime;
 
-        HabitModel myHabit = new HabitModel(name, timeFrame, originalTime, currentTime, expectedTime);
+        myHabit = new HabitModel(name, timeFrame, originalTime, expectedTime);
         String habitInfo = myHabit.printHabit();
 
         ((TextView) findViewById(R.id.answer)).setText(habitInfo);
-
     }
 
+    public void currentTimeSubmit(View V) {
+        EditText currentTimeView = (EditText) findViewById(R.id.todayHrs);
+        String currentTime = currentTimeView.getText().toString();
+        myHabit.currentTimes.add(Double.parseDouble(currentTime));
+        myHabit.printCurrentTimes();
+//        myHabit.printAverage();
+    }
+
+    /** Called when the user taps the Send button */
+    public void gainHabbit(View view) {
+        Intent intent = new Intent(this, gainHabbit.class);
+        startActivity(intent);
+    }
+
+    public void progress(View view) {
+        Intent intent = new Intent(this, progress.class);
+        startActivity(intent);
+    }
 }
